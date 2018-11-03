49baf1f9c621f23170478043e5e30227cec045f4
Yuanx1998 <592302815@qq.com>
2018-11-04 00:20:08 +0800

    study163_11.4

---
+++ miniprogram/app.js
@@ -0,0 +1,19 @@
+//app.js
+App({
+
+   
+  
+  onLaunch: function () {
+    
+
+    // console.log('应用启动'); 
+    
+    this.globalData = {}
+    
+  }
+  
+
+  
+
+
+})
---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,85 @@
+//index.js
+const app = getApp();
+const globalData = [];
+
+Page({
+  data: {
+    userInfo: {},
+    logged: false,
+    takeSession: false,
+    requestResult: '',
+    // sellWell: [],
+    // special: [],
+    mainPage:{},
+    title:[{
+      "text":"畅销好课",
+      "index":0
+    },{
+      "text":"专题",
+      "index":1
+    },{
+      "text":"办公效率",
+      "index":2
+    },{
+      "text":"职场发展",
+      "index":3
+    },{
+      "text":"语言留学",
+      "index":4
+    },{
+      "text":"金融财会",
+      "index":5
+    },{
+      "text":"职业之外",
+      "index":6
+    },],
+    ads:
+    [
+      {
+        "image": "../../images/ads/1.jpg"
+      },
+      {
+        "image": "../../images/ads/2.jpg"
+      },
+      {
+        "image": "../../images/ads/3.jpg"
+      },
+      {
+        "image": "https://edu-image.nosdn.127.net/e62efdae-0ba9-48bd-8cf7-ce521fcd2fb7.jpg?imageView&amp;crop=0_0_1242_573&amp;quality=100&amp;thumbnail=750y346&amp;type=webp"
+      },
+      {
+        "image": "https://edu-image.nosdn.127.net/c176d36b-e75f-4862-97a5-3f38a7a7ab9f.jpg?imageView&amp;crop=0_0_1242_573&amp;quality=100&amp;thumbnail=750y346&amp;type=webp"
+      },
+      {
+        "image": "https://edu-image.nosdn.127.net/beccf78b-6b92-4303-88d5-613b0e41395e.jpg?imageView&amp;crop=0_0_1242_573&amp;quality=100&amp;thumbnail=750y346&amp;type=webp"
+      },
+      {
+        "image": "https://edu-image.nosdn.127.net/809624c7-db3f-4f4d-bc9b-85a3e8835279.jpg?imageView&amp;crop=0_0_1242_573&amp;quality=100&amp;thumbnail=750y346&amp;type=webp"
+      }
+    ]
+  },
+
+  onLoad() {
+
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5bda9d1a58caf84108172bab/study163/mainPage',
+      success:(res) => {
+        // console.log("---res.data---")
+        // console.log(res.data)
+
+         this.setData({
+           mainPage : res.data       
+         })
+
+        console.log("---test---") 
+        console.log(this.data.mainPage)
+ 
+      },
+      error:(err) => {
+        console.log(err);
+      }
+    })
+
+  }
+  
+});
