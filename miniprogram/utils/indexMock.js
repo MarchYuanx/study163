f221ca1f6514bd3b60e1488fbdd8d033cf682f08
Yuanx1998 <592302815@qq.com>
2018-11-25 17:33:01 +0800

    utils

--- miniprogram/utils/indexMock.js
+++ miniprogram/utils/indexMock.js
@@ -1,5 +1,20 @@
+let indexMock = function(url){    
+      return new Promise((resolve,reject)=>{
+          wx.request({
+              url:url,
+              success(res){
+                  resolve(res.data)
+              },
+              fail(err){
+                  reject(err)
+              }
+          })
+      })
+  
+}
 
 
-// module.exports = {
-//   indexMock: indexMock
-// }
+
+module.exports = {
+    indexMock: indexMock
+}
